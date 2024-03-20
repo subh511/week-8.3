09145d5c4fd072105744d41b8667784b10328de1
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-03-20 13:51:19 +0530

    post req via header and authorization

--- src/FetchedAxios.jsx
+++ src/FetchedAxios.jsx
@@ -2,11 +2,27 @@
 
 import axios from "axios";
 async function FetchedAxios() {
+  // const response = await axios.get(`https://sum-server.100xdevs.com/todos`);
+  // console.log(response.data.todos.length);
 
-  const response = await axios.get(`https://sum-server.100xdevs.com/todos`);
+  const response = await axios.post(
+    ` https://httpdump.app/dumps/4d0a776d-ff71-45fc-a0ef-0138cfe2fb8c?a=b`,
+    {
+      username: "harkirat",
+      password: "1234567",
+    },
+    {
+      headers: {
+        Authorization: "Bearer 123",
+      },
+    }
+  );
   console.log(response.data.todos.length);
 
   return <div>Axios</div>;
 }
 
+//axios.put;
+//axios.delete;
+//axios.post
 export default FetchedAxios;
