708a5e06f95ad226d6136806b54c261d12ffd5d1
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-03-20 12:40:57 +0530

    modified fetched in cleaned way

--- src/Fetch.jsx
+++ src/Fetch.jsx
@@ -1,10 +1,22 @@
 //import React from 'react'
 
-function Fetch() {
-  return (
-    <div>
-    </div>
-  )
+//import { useEffect } from "react";
+
+async function Fetch() {
+
+
+  //1st way of fetching data;
+
+  //   fetch("https://sum-server.100xdevs.com/todos").then(async (response) => {
+  //     const json = await response.json();
+  //     console.log(json.todos.length);
+  //   });
+
+  const response = await fetch(`https://sum-server.100xdevs.com/todos`)
+  const json = await response.json();
+  console.log(json.todos.length)
+
+  return <div></div>;
 }
 
-export default Fetch
+export default Fetch;
